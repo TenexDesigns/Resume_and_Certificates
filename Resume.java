52167f51eae3e96c0c4a7def56932fb8fa7ed2d6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-30 15:57:58 +0300

    Create Resume.java

---
+++ Resume.java
@@ -0,0 +1,95 @@
+Certainly, here's a resume for a Fullstack Developer with a focus on JavaScript and Java, including essential details and areas of expertise:
+
+```plaintext
+[Your Name]
+[Your Address]
+[City, State ZIP Code]
+[Your Email Address]
+[Your Phone Number]
+[LinkedIn Profile URL]
+[GitHub Profile URL]
+
+Objective:
+--------------
+A highly motivated and adaptable Fullstack Developer with a strong passion for creating robust and efficient web applications. Proficient in JavaScript, Java, Spring, React, Next.js, and SQL. Seeking opportunities to contribute to dynamic development teams and leverage technical expertise to build innovative software solutions.
+
+Education:
+--------------
+Bachelor of Science in Telecommunications and Information Engineering
+Jomo Kenyatta University of Agriculture and Technology
+
+Skills:
+--------------
+Frontend Technologies:
+- JavaScript (ES6+)
+- React.js
+- Next.js
+- HTML5
+- CSS3
+- Tailwind CSS
+
+Backend Technologies:
+- Java
+- Spring Framework
+- Spring Boot
+- RESTful API Development
+- PostgreSQL
+
+Tools and Technologies:
+- Git and GitHub
+- Docker
+- SQL Databases
+- Visual Studio Code
+- Agile/Scrum Methodology
+- Test-Driven Development (TDD)
+
+Professional Experience:
+--------------
+Freelance Fullstack Developer
+[Dates of Freelance Work]
+
+- Collaborated with clients to gather project requirements, design architecture, and deliver customized web applications.
+- Utilized JavaScript, React.js, Next.js, and Tailwind CSS to build responsive and user-friendly frontend interfaces.
+- Designed and implemented RESTful APIs using Java and Spring Boot to handle backend functionalities.
+- Integrated PostgreSQL databases for data storage and retrieval, implementing CRUD operations.
+- Maintained version control using Git, ensuring codebase integrity and collaboration with team members.
+- Demonstrated proficiency in code optimization, performance tuning, and debugging to enhance application speed and reliability.
+- Delivered high-quality web solutions within project deadlines, resulting in client satisfaction and repeat business.
+
+Projects:
+--------------
+Project 1: [Project Name]
+- Description: [Brief project description]
+- Live Demo: [Live demo URL]
+- GitHub Repository: [GitHub repo URL]
+
+Project 2: [Project Name]
+- Description: [Brief project description]
+- Live Demo: [Live demo URL]
+- GitHub Repository: [GitHub repo URL]
+
+Portfolio Website:
+- Designed and developed a personal portfolio website to showcase completed projects and skills.
+- Live Website: [Portfolio website URL]
+- GitHub Repository: [GitHub repo URL]
+
+Certifications:
+--------------
+- [Certification Name], [Certifying Authority], [Date]
+- [Certification Name], [Certifying Authority], [Date]
+
+Professional Memberships:
+--------------
+- Member, [Professional Association Name]
+
+Languages:
+--------------
+- English (Fluent)
+- [Additional Languages, if applicable]
+
+References:
+--------------
+Available upon request.
+```
+
+Please replace the placeholders with your actual information, and feel free to add or modify sections as needed. Additionally, consider including any other certifications, awards, or achievements that you may have in your field.
